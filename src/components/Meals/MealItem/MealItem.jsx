74dd13da7a831c3bc5e1f8889b49b0981ce05198
Abhishek-mishra-bit <abhishekmi1249@gmail.com>
2024-07-26 11:58:17 +0530

    Adding style in meals list

---
+++ src/components/Meals/MealItem/MealItem.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/prop-types */
+import classes from "./MealItem.module.css";
+import MealItemForm from "./MealItemForm";
+const MealItem = (props) => {
+  const price = `$${props.price.toFixed(2)}`;
+
+  return (
+    <li className={classes.meal}>
+      <div>
+        <h3 className={props.name}>{props.name}</h3>
+        <div className={props.description}>{props.description}</div>
+        <div className={classes.price}>{price}</div>
+      </div>
+      <div></div>
+    </li>
+  );
+};
+export default MealItem;
