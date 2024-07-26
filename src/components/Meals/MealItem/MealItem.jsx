ed8843b3283ed0c9dcf9a0594ebb8df93d2a2c6c
Abhishek-mishra-bit <abhishekmi1249@gmail.com>
2024-07-26 13:11:49 +0530

    Adding Add button and form section to add the quatity in meals list

--- src/components/Meals/MealItem/MealItem.jsx
+++ src/components/Meals/MealItem/MealItem.jsx
@@ -11,7 +11,9 @@ const MealItem = (props) => {
         <div className={props.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
       </div>
-      <div></div>
+      <div>
+        <MealItemForm />
+      </div>
     </li>
   );
 };
